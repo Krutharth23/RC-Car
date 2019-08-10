3b5524c438913375f45240e0d17245e322ed7ec3
LAPTOP-8VSLMRQD\asus <ckrutharth23@gmail.com>
2019-08-10 12:37:10 +0530

    Create Remote1.ino

---
+++ Remote1/Remote1.ino
@@ -0,0 +1,41 @@
+// setting up the joysticks
+int joystick1=A0;
+int joystick2=A1;
+
+// variables tostoret the value of the joysticks
+int joystick1Val;
+int joystick2Val;
+
+//int = state;
+
+void setup() {
+  // setting baudrate for BT module
+  Serial.begin(38400);
+  
+  // intilising the joysticks
+  pinMode(joystick1, INPUT);
+  pinMode(joystick2, INPUT);
+}
+
+void loop() {
+  // reading the value from the joysticks
+  joystick1Val = analogRead(joystick1);
+  joystick2Val = analogRead(joystick2);
+
+  // sending the approiate value as per the joystick
+  if (joystick1Val > 700){
+    Serial.write(1);
+  }
+  else if (joystick1Val < 300){
+    Serial.write(2);
+  }
+  else if (joystick2Val < 300){
+    Serial.write(3);
+  }
+  else if (joystick2Val > 700){
+    Serial.write(4);
+  }
+  else{
+    Serial.write(0);
+  }
+}
